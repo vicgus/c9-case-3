4fbef9a9ca3cba4f947a95bf9ab476e5b854391f
vicgus <victor.gsson@gmail.com>
2018-06-12 14:02:41 +0200

    Fifth
    With salongdata

--- src/App.js
+++ src/App.js
@@ -7,43 +7,6 @@ import down from './pics/down.png';
 import DropChild from './dropdownchild.js';
 import Sibling from './sibling.js';
 
-
-let defaultStyle = {
-  width: '375px',
-  fontSize: '5'
-};
-
-
-let salonger = [
-  {
-    name: 'Sax o fön',
-    price: 320,
-    address: 'Rådmansgatan 46',
-    description: 'Lorem ipsum...'
-  },
-  {
-    name: 'Hårizont',
-    price: 520,
-    address: 'Götgatan 1',
-    description: 'Ipsum lorem...'
-  },
-  {
-    name: 'Hårhuset',
-    price: 220,
-    address: 'Tätorpsvägen 45',
-    description: 'Sumip emrol...'
-  },
-  {
-    name: 'Hårhuset',
-    price: 220,
-    address: 'Sergels Torg 4005',
-    description: 'Emrol sumip...'
-  },
-];
-
-console.log(salonger)
-
-
 class MenuBar extends React.Component{
   render() {
     return(
@@ -62,17 +25,43 @@ let fakeServerData = {
   salong: {
     name: 'Sax o Fön',
     price: 320
+  },
+  salong: {
+    name: 'Hårizont',
+    price: 520
+  },
+  salong: {
+    name: 'Hårhuset',
+    price: 520
+  },
+  salong: {
+    name: 'Hair & Nu',
+    price: 250
+  },
+  salong: {
+    name: 'Hårley Davidson',
+    price: 900
   }
 };
+
+class Salong extends React.Component {
+  render() {
+    return(
+      <div className='listitem'>
+        <h1>{this.props.name} </h1>
+        <h1>{this.props.price} </h1>
+      </div>
+    )
+  }
+}
 
 class DropDownParent extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {range: ''};
+    this.state = {range: '', serverData: {}};
     this.changeRange = this.changeRange.bind(this);
   }
 
-  //EXPORT NEWRANGE??? LÖSNING???
   changeRange(newRange) {
     this.setState({
       range: newRange
@@ -81,14 +70,19 @@ class DropDownParent extends React.Component{
   }
 
   componentDidMount() {
-    this.setState({serverData: fakeServerData})
+    setTimeout(() => {
+    this.setState({serverData: fakeServerData});
+    }, 1000);
   }
 
   render() {
     return <div>  
               <DropChild range={this.state.range} onClick={this.changeRange} />
-              <Sibling range = {this.state.range} onClick={this.changeRange} />
-
+              {this.state.serverData.salong &&
+              <h1>{this.state.serverData.salong.name}</h1>}
+              <Salong price={this.state.serverData.salong && 
+                            this.state.serverData.salong.price}/>
+               {/* <Sibling range = {this.state.range} onClick={this.changeRange} /> */}
             </div>
   } 
 }
